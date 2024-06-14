f7e0a926b58ef9cdf44ee4c9b6fb99a60dc48a91
telamonian <telamonian@users.noreply.github.com>
2024-06-14 12:45:38 -0400

    add stubbed python code

--- {{cookiecutter.project_slug}}/src/{{cookiecutter.project_slug}}/__init__.py
+++ {{cookiecutter.project_slug}}/src/{{cookiecutter.project_slug}}/__init__.py
@@ -3,3 +3,6 @@
 __author__ = """{{ cookiecutter.full_name }}"""
 __email__ = '{{ cookiecutter.email }}'
 __version__ = '{{ cookiecutter.version }}'
+
+import NODE_CLASS_MAPPINGS
+import NODE_DISPLAY_NAME_MAPPINGS
