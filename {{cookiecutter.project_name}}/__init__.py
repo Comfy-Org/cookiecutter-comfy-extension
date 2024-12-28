416184ed58d02a16b3f0d0c9b73ef3d06fcdea6d
Robin Huang <robin.j.huang@gmail.com>
2024-12-28 12:00:35 -0800

    Fixed format.

--- {{cookiecutter.project_name}}/__init__.py
+++ {{cookiecutter.project_name}}/__init__.py
@@ -12,9 +12,9 @@ __author__ = """{{cookiecutter.full_name}}"""
 __email__ = "{{cookiecutter.email}}"
 __version__ = "{{cookiecutter.version}}"
 
-from .nodes import NODE_CLASS_MAPPINGS
-from .nodes import NODE_DISPLAY_NAME_MAPPINGS
+from .src.nodes import NODE_CLASS_MAPPINGS
+from .src.nodes import NODE_DISPLAY_NAME_MAPPINGS
 
 {% if cookiecutter.__include_web -%}
-WEB_DIRECTORY = "web"
+WEB_DIRECTORY = "./web"
 {%- endif %}
