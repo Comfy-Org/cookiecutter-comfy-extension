3a3ebd38d9c09c145a024822740a8ac36d52fd94
Robin Huang <robin.j.huang@gmail.com>
2024-12-28 18:16:56 -0800

    Use relative imports.

--- {{cookiecutter.project_slug}}/__init__.py
+++ {{cookiecutter.project_slug}}/__init__.py
@@ -12,8 +12,8 @@ __author__ = """{{cookiecutter.full_name}}"""
 __email__ = "{{cookiecutter.email}}"
 __version__ = "{{cookiecutter.version}}"
 
-from src.{{cookiecutter.project_slug}}.nodes import NODE_CLASS_MAPPINGS
-from src.{{cookiecutter.project_slug}}.nodes import NODE_DISPLAY_NAME_MAPPINGS
+from .src.{{cookiecutter.project_slug}}.nodes import NODE_CLASS_MAPPINGS
+from .src.{{cookiecutter.project_slug}}.nodes import NODE_DISPLAY_NAME_MAPPINGS
 
 {% if cookiecutter.__include_web -%}
 WEB_DIRECTORY = "./web"
